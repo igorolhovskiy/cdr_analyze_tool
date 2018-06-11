17628196a0e41e84ffcc295735c3a5d4970033fb
samael33 <IgorOlhovskiy@gmail.com>
2018-06-11 14:04:51 +0300

    ...

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$astpp_config = array(
+    'host' => 'localhost',
+    'username' => 'astpp_user',
+    'password' => 'astpp_pass',
+    'database' => 'astpp'
+);
+$local_config = array(
+    'host' => 'localhost',
+    'username' => 'db_user',
+    'password' => 'db_pass',
+    'database' => 'cdr_analyze_tool'
+);
+?>
